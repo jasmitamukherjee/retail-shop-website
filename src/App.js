baaf783a89fdaded32d5c78aa51156290c168643
jasmitamukherjee <jasmitamukherjee4@gmail.com>
2024-07-07 16:47:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Home from './components/Home'
-import Products from './components/Products'
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
+import OurShops from './components/OurShops';
+import ContactUs from './components/ContactUs';
+import Products from './components/Products';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/our-shops' element={<OurShops/>}/>
+        <Route path='/contact-us' element={<ContactUs/>}/>
         <Route path='/products' element={<Products/>}/>
+
 
       </Routes>
     </Router>
